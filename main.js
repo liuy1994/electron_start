8e91e4b1d1667eb97114ecceb49667c2e8f6502f
liuyong <liuyong@evision.ai>
2020-08-18 18:07:20 +0800

    react + ts + antd + less + electron

--- main.js
+++ main.js
@@ -1,43 +1,52 @@
-// Modules to control application life and create native browser window
+// 引入electron并创建一个Browserwindow
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
+const url = require('url')
+
+// 保持window对象的全局引用,避免JavaScript对象被垃圾回收时,窗口被自动关闭.
+let mainWindow
 
 function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+//创建浏览器窗口,宽高自定义具体大小你开心就好
+  mainWindow = new BrowserWindow({width: 800, height: 600})
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  /*
+   * 加载应用-----  electron-quick-start中默认的加载入口
+    mainWindow.loadURL(url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+    }))
+  */
+  // 加载应用----适用于 react 项目
+  mainWindow.loadURL('http://localhost:3000/');
 
-  // Open the DevTools.
+  // 打开开发者工具，默认不打开
   // mainWindow.webContents.openDevTools()
+
+  // 关闭window时触发下列事件.
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow()
-  
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+// 当 Electron 完成初始化并准备创建浏览器窗口时调用此方法
+app.on('ready', createWindow)
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+// 所有窗口关闭时退出应用.
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+  // macOS中除非用户按下 `Cmd + Q` 显式退出,否则应用与菜单栏始终处于活动状态.
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+app.on('activate', function () {
+  // macOS中点击Dock图标时没有已打开的其余应用窗口时,则通常在应用中重建一个窗口
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+// 你可以在这个脚本中续写或者使用require引入独立的js文件.
+
